5b78de201c07737358928459c044458062cb99b0
DavidMoraV <david.mora@ulead.ac.cr>
2025-02-12 16:11:15 -0600

    datos guardados

--- practica.py
+++ practica.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get("/user/{user_id}")
+# Endpoint GET
+@app.get("/get-user/{user_id}")
 async def get_user(user_id: int):
-    return {"user_id": user_id, "name": "User name here"}
+    return {"user_id": user_id, "username": "test_user"}
 
+# Endpoint POST
+class User(BaseModel):
+    username: str
+    email: str
+
+@app.post("/create-user")
+async def create_user(user: User):
+    return {"message": f"User {user.username} created with email {user.email}"}
 
