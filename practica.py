8ba21195d75f3245569e8744abcfaf492a1a3f17
DavidMoraV <david.mora@ulead.ac.cr>
2025-02-12 17:02:14 -0600

    user

--- practica.py
+++ practica.py
@@ -6,7 +6,7 @@ app = FastAPI()
 # Endpoint GET
 @app.get("/get-user/{user_id}")
 async def get_user(user_id: int):
-    return {"user_id": user_id, "username": "test_user"}
+    return {"user_id": user_id, "username": "David"}
 
 # Endpoint POST
 class User(BaseModel):
